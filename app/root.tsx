a01bdc0def7e351ea995afa78403c6af4df4cf6f
Vytas <cepulis.vytas@gmail.com>
2024-04-30 22:32:26 +0300

    added title to root head

--- app/root.tsx
+++ app/root.tsx
@@ -57,6 +57,7 @@ export default function App() {
   return (
     <html>
       <head>
+        <title>EZ Alerts app</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="preconnect" href="https://cdn.shopify.com/" />
